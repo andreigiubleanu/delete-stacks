cff4bd71d21010f5316560b1c127387f22ee120a
Andrei Giubleanu <andrei.giubleanu@gmail.com>
2023-09-01 05:40:30 +0000

    generated list of tags

--- delete_sylva_stacks.sh
+++ delete_sylva_stacks.sh
@@ -11,8 +11,7 @@ THRESHOLD=$(( NUM_DAYS *  24 * 60 * 60 ))
 TAGS=$(mktemp)
 EXCLUDED_TAGS=$(mktemp)
 ALL_PROJECTS=$(openstack project list -f json -c Name | jq -r .[].Name)
-ALL_STACKS=$(openstack stack list -f json -c ID -c "Stack Name" -c Project -c "Creation Time" | jq -r '.[]' | jq -r '.ID,."Stack Name",.Project,."Creation Time"' | xargs -n 4)
-# | grep -E '^(management|workload|capo)-cluster')
+ALL_STACKS=$(openstack stack list -f json -c ID -c "Stack Name" -c Project -c "Creation Time" | jq -r '.[]' | jq -r '.ID,."Stack Name",.Project,."Creation Time"' | xargs -n 4 | grep -E '^(management|workload|capo)-cluster')
 
 while read stack_id stack_name project_name creation_time; do
 	creation_time_epoch=$(date --date="${creation_time}" +%s)
@@ -20,9 +19,6 @@ while read stack_id stack_name project_name creation_time; do
 	seconds_elapsed=$(( now - creation_time_epoch ))
 	if [[ "${seconds_elapsed}" -lt "${THRESHOLD}" ]] ; then
 		openstack stack show "${stack_id}" -f json -c tags | jq -r '.tags[]' >> "${TAGS}"
-        	if [ "$?" -ne 0 ] ; then
-			echo "Unable to delete stack "${stack_name}" with "${stack_id}". Please proceed with manual deletion."
-		fi
 	else
 		
 		openstack stack show "${stack_id}" -f json -c tags | jq -r '.tags[]' >> "${EXCLUDED_TAGS}"
